75fadf2b81ba4351715bbb136b73c99803425e4a
Sohojware <sohojwareltd>
2025-12-02 15:50:28 +0600

    issue

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -67,37 +67,37 @@ class LoginController extends Controller
 
     protected function authenticated(Request $request, $user)
     {
-        $deviceId = $request->cookie('device_id') ?? Str::uuid()->toString();
+        // $deviceId = $request->cookie('device_id') ?? Str::uuid()->toString();
 
-        $existingDevice = $user->devices()->where('device_id', $deviceId)->first();
+        // $existingDevice = $user->devices()->where('device_id', $deviceId)->first();
 
-        if (!$existingDevice) {
-            if ($user->devices()->count() >= 1) {
-                auth()->logout();
-                Session::invalidate();
-                Session::regenerateToken();
+        // if (!$existingDevice) {
+        //     if ($user->devices()->count() >= 1) {
+        //         auth()->logout();
+        //         Session::invalidate();
+        //         Session::regenerateToken();
 
-                return redirect()->route('login')->withErrors([
-                    'email' => 'You are already logged in on 1 devices.',
-                ]);
-            }
+        //         return redirect()->route('login')->withErrors([
+        //             'email' => 'You are already logged in on 1 devices.',
+        //         ]);
+        //     }
 
-            $user->devices()->create([
-                'device_id' => $deviceId,
-                'device_agent' => $request->userAgent(),
-                'ip_address' => $request->ip(),
-                'session_id' => Session::getId(),
-            ]);
-        } else {
-            $existingDevice->update([
-                'session_id' => Session::getId(),
-                'ip_address' => $request->ip(),
-                'device_agent' => $request->userAgent()
-            ]);
-        }
+        //     $user->devices()->create([
+        //         'device_id' => $deviceId,
+        //         'device_agent' => $request->userAgent(),
+        //         'ip_address' => $request->ip(),
+        //         'session_id' => Session::getId(),
+        //     ]);
+        // } else {
+        //     $existingDevice->update([
+        //         'session_id' => Session::getId(),
+        //         'ip_address' => $request->ip(),
+        //         'device_agent' => $request->userAgent()
+        //     ]);
+        // }
 
-        // Ensure cookie is set
-        cookie()->queue('device_id', $deviceId, 525600); // 1 year
+        // // Ensure cookie is set
+        // cookie()->queue('device_id', $deviceId, 525600); // 1 year
     }
     public function __construct()
     {
